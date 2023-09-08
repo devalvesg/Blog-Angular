3f24815c3b072ece3051cf14f2baa5ab72fa2961
Gabriel Alves <gabrieldamasceno881@outlook.com>
2023-09-08 17:16:50 -0300

    Criando os componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TituloPrincipalComponent } from './components/titulo-principal/titulo-principal.component';
+import { CardPrincipalComponent } from './components/card-principal/card-principal.component';
+import { CardsSecundariosComponent } from './components/cards-secundarios/cards-secundarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TituloPrincipalComponent,
+    CardPrincipalComponent,
+    CardsSecundariosComponent
   ],
   imports: [
     BrowserModule,
