b815ada8a2b81f27cd491bf8dac5389b1c5c6c4f
Gabriel Alves <gabrieldamasceno881@outlook.com>
2023-09-10 22:00:46 -0300

    Parte visual finalizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { TituloPrincipalComponent } from './components/titulo-principal/titulo-principal.component';
-import { CardPrincipalComponent } from './components/card-principal/card-principal.component';
-import { CardsSecundariosComponent } from './components/cards-secundarios/cards-secundarios.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from 'src/app/components/navbar/navbar.component';
+import { TituloPrincipalComponent } from 'src/app/components/titulo-principal/titulo-principal.component';
+import { CardPrincipalComponent } from 'src/app/components/card-principal/card-principal.component';
+import { CardsSecundariosComponent } from 'src/app/components/cards-secundarios/cards-secundarios.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    TituloPrincipalComponent,
-    CardPrincipalComponent,
-    CardsSecundariosComponent
+    AppComponent, HomeComponent, NavbarComponent,TituloPrincipalComponent, CardPrincipalComponent, CardsSecundariosComponent
   ],
   imports: [
     BrowserModule,
